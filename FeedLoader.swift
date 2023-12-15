2a14b9d7ec2fa1e0e7dc57135104e62755d1c73f
Mario Elorza <mario.elorza@gmail.com>
2023-12-15 14:55:11 -0300

    Creates FeedLoader protocol boundary

---
+++ FeedLoader.swift
@@ -0,0 +1,14 @@
+//
+//  FeedLoader.swift
+//  EssentialFeed
+//
+//  Created by Mario Elorza on 15-12-23.
+//
+
+import Foundation
+
+typealias LoadFeedResult: Result([FeedItem, Error])
+
+protocol FeedLoader {
+    func load(completion: @escaping LoadFeedResult -> Void)
+}
