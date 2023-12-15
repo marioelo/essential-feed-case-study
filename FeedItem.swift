8a010e18c864316e4c8f6b8bdd9270456e89bb1f
Mario Elorza <mario.elorza@gmail.com>
2023-12-15 08:34:17 -0300

    Creates Feed Item model

---
+++ FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  EssentialFeed
+//
+//  Created by Mario Elorza on 15-12-23.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
