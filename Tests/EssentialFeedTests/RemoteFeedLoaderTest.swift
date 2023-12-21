3c729ca55f30268313333f8475d087cdb3fc2fd2
Mario Elorza <mario.elorza@gmail.com>
2023-12-21 17:33:49 -0300

    Delivers items array on 200 HTTPResponse with JSON items

--- Tests/EssentialFeedTests/RemoteFeedLoaderTest.swift
+++ Tests/EssentialFeedTests/RemoteFeedLoaderTest.swift
@@ -63,6 +63,41 @@ final class RemoteFeedLoaderTest: XCTestCase {
         }
     }
     
+    func test_load_deliversItemsOn200HTTPResponseWitrhJSONItems() {
+        let (sut, client) = makeSUT()
+        
+        let item1 = FeedItem(id: UUID(), 
+                             description: nil,
+                             location: nil,
+                             imageURL: URL(string: "https://a-url.com")!)
+        
+        let item1JSON = [
+            "id": item1.id.uuidString,
+            "image": item1.imageURL.absoluteString
+        ]
+        
+        let item2 = FeedItem(id: UUID(),
+                             description: "a description",
+                             location: "a location",
+                             imageURL: URL(string: "https://a-url.com")!)
+        
+        let item2JSON = [
+            "id": item2.id.uuidString,
+            "description": item2.description,
+            "location": item2.location,
+            "image": item2.imageURL.absoluteString
+        ]
+        
+        let itemsJSON = [
+            "items": [item1JSON, item2JSON]
+        ]
+        
+        expect(sut, toCompleteWith: .success([item1, item2])) {
+            let json = try! JSONSerialization.data(withJSONObject: itemsJSON)
+            client.complete(withStatusCode: 200, data: json)
+        }
+    }
+    
     func test_load_requestsDataFromURLTwice() {
         let url = URL(string: "https://a-url.com")!
         let (sut, client) = makeSUT(url: url)
