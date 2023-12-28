877a923264335dc4faed1da6f88e0ee84b826fff
Mario Elorza <mario.elorza@gmail.com>
2023-12-27 23:33:44 -0300

    Fix overriding setUp and tearDown 'class' methods

--- Tests/EssentialFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
+++ Tests/EssentialFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
@@ -10,12 +10,12 @@ import EssentialFeed
 
 final class URLSessionHTTPClientTests: XCTestCase {
     
-    override class func setUp() {
+    override func setUp() {
         super.setUp()
         URLProtocolStub.startInterceptingRequests()
     }
     
-    override class func tearDown() {
+    override func tearDown() {
         super.tearDown()
         URLProtocolStub.stopInterceptingRequests()
     }
